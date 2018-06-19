5e3fd673c1b1afaaa243f3a287c7481cce153fb0
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2018-06-18 18:41:28 -0700

    Add Rekit

--- scripts/start.js
+++ scripts/start.js
@@ -105,3 +105,38 @@ choosePort(HOST, DEFAULT_PORT)
     }
     process.exit(1);
   });
+
+/* Starts Rekit. Taken from https://medium.freecodecamp.org/using-rekit-studio-in-an-existing-react-project-39713d9667b */
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const rekitStudioMiddleWare = require('rekit-studio/middleware');
+const fallback = require('express-history-api-fallback');
+const pkgJson = require('../package.json');
+
+function startStudioServer() {
+  console.log('Starting Rekit Studio...');
+  const app = express();
+  const server = http.createServer(app);
+  const root = path.join(__dirname, '../node_modules/rekit-studio/dist');
+  app.use(rekitStudioMiddleWare()(server, app));
+  app.use(express.static(root));
+  app.use(fallback('index.html', { root }));
+
+  // Other files should not happen, respond 404
+  app.get('*', (req, res) => {
+    console.log('Warning: unknown req: ', req.path);
+    res.sendStatus(404);
+  });
+
+  const port = pkgJson.rekit.studioPort;
+  server.listen(port, (err) => {
+    if (err) {
+      console.error(err);
+    }
+
+    console.log(`Studio server is listening at http://localhost:${port}/`);
+  });
+}
+startStudioServer();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { AppContainer } from 'react-hot-loader';
+import { render } from 'react-dom';
+import configStore from './common/configStore';
+import routeConfig from './common/routeConfig';
+import Root from './Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configStore();
+
+function renderApp(app) {
+  render(
+    <AppContainer>
+      {app}
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+renderApp(<Root store={store} routeConfig={routeConfig} />);
+
+// Hot Module Replacement API
+/* istanbul ignore if  */
+if (module.hot) {
+  module.hot.accept('./common/routeConfig', () => {
+    const nextRouteConfig = require('./common/routeConfig').default; // eslint-disable-line
+    renderApp(<Root store={store} routeConfig={nextRouteConfig} />);
+  });
+  module.hot.accept('./Root', () => {
+    const nextRoot = require('./Root').default; // eslint-disable-line
+    renderApp(<Root store={store} routeConfig={routeConfig} />);
+  });
+}
